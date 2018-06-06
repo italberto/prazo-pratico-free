e2ebb218fa7a8e273ed0850b9761885bee665708
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2018-06-06 11:16:51 -0300

    cookie setting and getting working

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import webapp2
 import jinja2
+import json
+import base64
 #funcoes de login
 import sigaa
 
@@ -9,6 +11,7 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
 class MainPage(webapp2.RequestHandler):
+    """index page"""
     def get(self):
         t = jinja_env.get_template('index.html')
         self.response.out.write(t.render())
@@ -16,6 +19,7 @@ class MainPage(webapp2.RequestHandler):
 
 
 class LoginPage(webapp2.RequestHandler):
+    """Login page"""
     def get(self, error="", user=""):
         t = jinja_env.get_template('login.html')
         self.response.out.write(t.render(error=error, username=user))
@@ -26,15 +30,31 @@ class LoginPage(webapp2.RequestHandler):
 
         #if the data is full correctly
         if username and password:
-            if sigaa.login(username, password):
-                self.response.out.write('OK')
+            #make login
+            login = sigaa.login(username, password)
             #if user or password is wrong
+            if login:
+                login_json = json.dumps(login)
+                self.response.headers['Set-Cookie'] = 'user='+base64.b64encode(login_json)
+                self.redirect('/home')
+
             else:
                 self.get("Senha ou usuario invalidos.", username)
                 pass
 
+class Home(webapp2.RequestHandler):
+    def get(self):
+        cookie_value = self.request.cookies.get('user')
+        cookie_value = base64.b64decode(cookie_value)
+        cookie = json.loads(cookie_value)
+        print cookie
+
+        if cookie_value:
+
+            pass
 
 
 
 
-app = webapp2.WSGIApplication([('/', MainPage), ('/login', LoginPage)])
+
+app = webapp2.WSGIApplication([('/', MainPage), ('/login', LoginPage), ('/home', Home)])
--- sigaa.py
+++ sigaa.py
@@ -1,5 +1,26 @@
 import urllib2, urllib
 
+def get_classes_info(html_home):
+    """return classes objects exhtracted from homepage_html"""
+    #list of class elements
+    classes = []
+    classes_html_elements = html_home.split('<input type="hidden" name="form_acessarTurmaVirtual')[1:]
+
+    for class_html in classes_html_elements:
+        class_obj = {} # class object
+        #class id extrating
+        class_html_i = class_html.find('<input type="hidden" value="') + 28
+        class_html_j = class_html.find('" name="idTurma" />')
+        class_obj['id'] = class_html[class_html_i:class_html_j]
+
+        #class name extrating
+        class_html_i = class_html.find('return false">') + 14
+        class_html_j = class_html.find('</a><input type="hidden"')
+        class_obj['name'] = class_html[class_html_i:class_html_j]
+
+        classes.append(class_obj)
+    return classes
+
 def generate_new_cookie():
     """return a valid virgin cookie"""
     response = urllib2.urlopen('https://sigaa.ufpi.br/sigaa/verTelaLogin.do')
@@ -17,5 +38,14 @@ def login(username, password):
 
     html = urllib2.urlopen(request).read()
 
-    if "rio e/ou senha inv" not in html:
-        return html
+    if ("rio e/ou senha inv" not in html) and 'username: "' in html:
+        #user object
+        user = {}
+
+        username_i = html.find('username: "') + 11
+        username_f = html.find('", //pode ser retornado por funcao ou uma string')
+
+        user['username'] = html[username_i:username_f]
+        user['classes'] = get_classes_info(html)
+
+        return user
