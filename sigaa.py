2da6feb3dd8c3a4703d6a0e328519eeddf6e7b3f
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2018-06-08 08:39:06 -0300

    adding ssl to encrypt the data.

--- sigaa.py
+++ sigaa.py
@@ -33,8 +33,12 @@ def get_class_html(cookie, id):
 
             #extrair date_hora
             activity_html_i = activity_html.find('<span>') + 6
-            activity_html_f = activity_html.find('</div>')
-            activity['prazo'] = activity_html[activity_html_i:activity_html_f]
+            #verify if a deadline exist
+            if not(activity_html_i == 5):
+                activity_html_f = activity_html.find('</div>')
+                activity['prazo'] = activity_html[activity_html_i:activity_html_f]
+            else:
+                activity['prazo'] = "Nao possui prazo de validade."
 
             #acicionando a lista de atividades
             activities.append(activity)
